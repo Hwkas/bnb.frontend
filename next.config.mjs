6af464aba97f328a000dd35584183c46a74aa168
Amaan Saifi <amaan.saifi999@gmail.com>
2024-08-14 20:48:04 +0530

    fix: images not showing

--- next.config.mjs
+++ next.config.mjs
@@ -1,23 +1,24 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        remotePatterns: [
-            {
-                protocol: "http",
-                hostname: `${process.env.NEXT_PUBLIC_HOST_NAME}`,
-                port: `${process.env.NEXT_PUBLIC_PORT}`,
-                pathname: "/**",
-            }
-        ]
+        // TODO i guess here just need to configure cache in nginx.conf
+        unoptimized: true,
+        // remotePatterns: [
+        //     {
+        //         protocol: "http",
+        //         hostname: `${process.env.NEXT_PUBLIC_HOST_NAME}`,
+        //         port: `${process.env.NEXT_PUBLIC_PORT}`,
+        //         pathname: "/**",
+        //     },
+        // ],
     },
     experimental: {
         serverActions: {
             allowedOrigins: [
-                "localhost:3000",
-                `${process.env.NEXT_PUBLIC_HOST_NAME}:${process.env.NEXT_NGINX_PORT}`
+                `${process.env.NEXT_PUBLIC_HOST_NAME}:${process.env.NEXT_NGINX_PORT}`,
             ],
-        }
-    }
+        },
+    },
 };
 
 export default nextConfig;
