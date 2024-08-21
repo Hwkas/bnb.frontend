bd98c707b4906d4bd14759e98e9c814933499517
Amaan Saifi <amaan.saifi999@gmail.com>
2024-08-21 22:42:30 +0530

    for image load issue &  static files

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    output: 'standalone',
+    reactStrictMode: true,
+    swcMinify: true,
     images: {
         // TODO i guess here just need to configure cache in nginx.conf
         unoptimized: true,
@@ -13,6 +16,7 @@ const nextConfig = {
         // ],
     },
     experimental: {
+        // taint: true,
         serverActions: {
             allowedOrigins: [
                 `${process.env.NEXT_PUBLIC_HOST_NAME}:${process.env.NEXT_NGINX_PORT}`,
