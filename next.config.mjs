0702653e0a2113b8f5c7e8ee8d4b2e7ee369139f
Amaan Saifi <amaan.saifi999@gmail.com>
2024-10-21 22:02:55 +0530

    chore: restructure .env.local refactor: minor optimization of Dockerfile, docker-compose.yml, and next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -17,7 +17,7 @@ const nextConfig = {
         // taint: true,
         serverActions: {
             allowedOrigins: [
-                `${process.env.NEXT_PUBLIC_HOST_NAME}:${process.env.NEXT_NGINX_PORT}`,
+                `${process.env.NEXT_PUBLIC_HOST_NAME}`,
             ],
         },
     },
